a24fe206a1574fad88f1006390de5f370e37ddef
kikos <kikos@matchlab.org>
2025-11-04 16:38:10 +0800

    feat: update iframe

--- src/page/square/index.tsx
+++ src/page/square/index.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { useState } from 'react'
 
 const Square = () => {
+  const [isLoading, setIsLoading] = useState(true)
+
+  const handleIframeLoad = () => {
+    setIsLoading(false)
+  }
+
   return (
-    <div>Square</div>
+    <div className="relative w-full min-h-[calc(100vh-78px)] h-full">
+      {isLoading && (
+        <div className="absolute inset-0 flex h-full items-center justify-center bg-background">
+          <div className="flex flex-col items-center gap-3">
+            <div className="h-12 w-12 animate-spin rounded-full border-4 border-gray-300 border-t-primary"></div>
+            <p className="text-sm text-gray-500">Loading iFrame Page...</p>
+          </div>
+        </div>
+      )}
+      <iframe
+        src="http://localhost:5174/"
+        className="w-full h-full"
+        onLoad={handleIframeLoad}
+      />
+    </div>
   )
 }
 
